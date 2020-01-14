5dcf031e27a55017cdeda99c4fbc47b6d4486e3d
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-14 14:54:53 -0300

    Livros

--- src/main/java/br/com/batalhao/socialbooks/resources/LivrosResources.java
+++ src/main/java/br/com/batalhao/socialbooks/resources/LivrosResources.java
@@ -4,20 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.batalhao.socialbooks.domain.Livro;
 import br.com.batalhao.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping(value = "/livros")
 public class LivrosResources {
 
 	@Autowired
 	private LivrosRepository livrosRepository;
 
-	@GetMapping(value = "livros")
+	@GetMapping
 	public List<Livro> listar() {
 		return livrosRepository.findAll();
+	}
+
+	@PostMapping
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
 	}
 
 }
