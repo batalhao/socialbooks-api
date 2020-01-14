f9bda51c26910fd7f53f675653ee861b18f437b0
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-14 12:32:04 -0300

    Comentários

--- src/main/java/br/com/batalhao/socialbooks/domain/Comentario.java
+++ src/main/java/br/com/batalhao/socialbooks/domain/Comentario.java
@@ -4,14 +4,24 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonInclude(value = Include.NON_NULL)
+@Entity
+@Table(name = "comentarios")
 public class Comentario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String texto;
