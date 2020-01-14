85db89299644de97d314abff8130caee7e8d0c9a
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-14 10:31:14 -0300

    Primeira postagem

---
+++ src/main/java/br/com/batalhao/socialbooks/SocialbooksApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.batalhao.socialbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialbooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialbooksApiApplication.class, args);
+	}
+
+}
