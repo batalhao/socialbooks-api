d4a82e234b9ebd0bcb8f4a4d5463bf89e0ce53d9
Paulo R. Batalhão <paulo@javahub.dev>
2020-01-14 10:10:19 -0300

    Primeira postagem

---
+++ src/test/java/br/com/batalhao/socialbooks/SocialbooksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.batalhao.socialbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialbooksapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
